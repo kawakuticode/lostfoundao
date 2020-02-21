624fc5b3833833bad77d78d9077faf1311c58619
kawakuticode <kawakuticode@gmail.com>
2020-02-21 01:33:29 +0100

    add requirements.txt

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import webbrowser
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/LFDB.db'
 # logging.basicConfig()
